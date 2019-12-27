ef308e5d4cc6a760a35b504d28b04fe4eb8b43d3
Joe Pea <joe@trusktr.io>
2019-12-26 22:21:11 -0800

    - improve attribute handling code - add a feature so that when a behavior has a property awaitElementDefined set to true that life cycle methods will not be fired until the host element has been defined (upgraded into a custom element)

--- src/index.js
+++ src/index.js
@@ -53,17 +53,25 @@ Object.defineProperty(Element.prototype, 'behaviors', {
 class HasAttribute {
 	constructor() {
 		// TODO constructor confusing because this.ownerElement doesn't exist. Report to custom-attributes
-
-		this.observers = new Map()
 	}
 
 	connectedCallback() {
+		this.observers = new Map()
 		this.behaviors = this.ownerElement.behaviors
+		this.connectedBehaviors = new Set()
+		this.definePromises = new Map()
+		this.foo = true
 		this.changedCallback('', this.value)
 	}
 
 	disconnectedCallback() {
 		this.changedCallback(this.value, '')
+		this.observers && this.observers.clear()
+		this.connectedBehaviors && this.connectedBehaviors.clear()
+		if (this.definePromises) {
+			for (const [, promise] of this.definePromises) promise.cancel()
+			this.definePromises.clear()
+		}
 		delete this.behaviors
 	}
 
@@ -113,54 +121,82 @@ class HasAttribute {
 	handleDiff(removed, added) {
 		for (const name of removed) {
 			if (!elementBehaviors.has(name)) continue
-
-			const behavior = this.behaviors.get(name)
-
-			// TODO fire this disconnectedCallback only if the element is in a
-			// document, not if it merely has a parent (the naive easy way for
-			// now).
-			if (this.ownerElement.parentNode) {
-				behavior.disconnectedCallback()
-			}
-
-			// We can't rely on checking observedAttributes here because that
-			// could change after the fact, we only ever check it when we add
-			// the behavior. If it had observedAttributes, then it will have an
-			// observer.
-			if (this.observers.has(behavior)) {
-				this.destroyAttributeObserver(behavior)
-			}
-
-			this.behaviors.delete(name)
+			this.disconnectBehavior(name)
 		}
 
 		for (const name of added) {
 			if (!elementBehaviors.has(name)) continue
-
-			const Behavior = elementBehaviors.get(name)
-			const behavior = new Behavior(this.ownerElement)
-			this.behaviors.set(name, behavior)
-
-			// read observedAttributes first, in case anything external fires
-			// logic in the getter and expects it to happen before any
-			// lifecycle methods (f.e. a library like SkateJS)
-			const observedAttributes = behavior.constructor.observedAttributes
-
-			// TODO fire this connectedCallback only if the element is in a
-			// document, not if it merely has a parent (the naive easy way for
-			// now).
-			if (this.ownerElement.parentNode) {
-				behavior.connectedCallback()
-			}
-
-			if (Array.isArray(observedAttributes)) {
-				this.fireInitialAttributeChangedCallbacks(behavior)
-				this.createAttributeObserver(behavior)
-			}
+			this.connectBehavior(name)
 		}
 	}
 
+	connectBehavior(name) {
+		const Behavior = elementBehaviors.get(name)
+		const behavior = new Behavior(this.ownerElement)
+		this.behaviors.set(name, behavior)
+
+		// read observedAttributes first, in case anything external fires
+		// logic in the getter and expects it to happen before any
+		// lifecycle methods (f.e. a library like SkateJS)
+		const observedAttributes = behavior.constructor.observedAttributes
+
+		if (this.ownerElement.isConnected) {
+			const tagName = this.ownerElement.tagName
+
+			// if the element is a custom element and the behavior specifies to wait for it to be defined
+			if (behavior.awaitElementDefined && tagName.includes('-')) {
+				const promiseId = name + '_' + tagName
+				let promise = this.definePromises.get(promiseId)
+
+				if (!promise) {
+					promise = new CancelablePromise(customElements.whenDefined(tagName.toLowerCase()), {
+						rejectOnCancel: false,
+					})
+					this.definePromises.set(promiseId, promise)
+				}
+
+				promise.then(() => {
+					this.definePromises.delete(promiseId)
+					this.connectedBehaviors.add(behavior)
+					behavior.connectedCallback()
+				})
+			} else {
+				this.connectedBehaviors.add(behavior)
+				behavior.connectedCallback()
+			}
+		}
+
+		if (Array.isArray(observedAttributes) && observedAttributes.length) {
+			this.fireInitialAttributeChangedCallbacks(behavior, observedAttributes)
+			this.createAttributeObserver(behavior)
+		}
+	}
+
+	disconnectBehavior(name) {
+		const behavior = this.behaviors.get(name)
+
+		if (this.ownerElement.isConnected) {
+			if (this.connectedBehaviors.has(behavior)) {
+				behavior.disconnectedCallback()
+				this.connectedBehaviors.delete(behavior)
+			}
+
+			const promiseId = name + '_' + this.ownerElement.tagName
+			const promise = this.definePromises.get(promiseId)
+
+			if (promise) {
+				promise.cancel()
+				this.definePromises.delete(promiseId)
+			}
+		}
+
+		this.destroyAttributeObserver(behavior)
+
+		this.behaviors.delete(name)
+	}
+
 	destroyAttributeObserver(behavior) {
+		if (!this.observers.has(behavior)) return
 		this.observers.get(behavior).disconnect()
 		this.observers.delete(behavior)
 	}
@@ -172,11 +208,13 @@ class HasAttribute {
 	// element, only for disconnecting from all elements.
 	createAttributeObserver(behavior) {
 		const observer = new MutationObserver(records => {
+			if (!behavior.attributeChangedCallback) return
+
 			for (const record of records) {
 				behavior.attributeChangedCallback(
 					record.attributeName,
 					record.oldValue,
-					this.ownerElement.getAttribute(record.attributeName),
+					this.ownerElement.attributes[record.attributeName].value,
 				)
 			}
 		})
@@ -190,15 +228,77 @@ class HasAttribute {
 		this.observers.set(behavior, observer)
 	}
 
-	fireInitialAttributeChangedCallbacks(behavior) {
-		if (!Array.isArray(behavior.constructor.observedAttributes)) return
+	fireInitialAttributeChangedCallbacks(behavior, attributes) {
+		if (!behavior.attributeChangedCallback) return
 
-		for (const attr of Array.from(this.ownerElement.attributes)) {
-			if (!behavior.constructor.observedAttributes.includes(attr.name)) continue
-			if (behavior.attributeChangedCallback) behavior.attributeChangedCallback(attr.name, null, attr.value)
+		for (const name of attributes) {
+			if (this.ownerElement.hasAttribute(name))
+				behavior.attributeChangedCallback(name, null, this.ownerElement.attributes[name].value)
 		}
 	}
 }
+
+function Privates() {
+	const storage = new WeakMap()
+
+	return obj => {
+		let privates = storage.get(obj)
+		if (!privates) storage.set(obj, (privates = {}))
+		return privates
+	}
+}
+
+const _ = Privates()
+
+class CancelablePromise extends Promise {
+	constructor(executor, options) {
+		const rejectOnCancel = options ? options.rejectOnCancel : false
+		let originalReject
+
+		// if the first arg is a promise-like
+		if (typeof executor === 'object' && executor.then && executor.catch) {
+			const promise = executor
+
+			super((resolve, reject) => {
+				originalReject = reject
+
+				promise
+					.then(value => {
+						if (!this.canceled) resolve(value)
+					})
+					.catch(error => {
+						if (!this.canceled) reject(error)
+					})
+			})
+		} else {
+			super((resolve, reject) => {
+				originalReject = reject
+				executor(
+					value => {
+						if (!this.canceled) resolve(value)
+					},
+					error => {
+						if (!this.canceled) reject(error)
+					},
+				)
+			})
+		}
+
+		this.canceled = false
+		_(this).originalReject = originalReject
+		_(this).rejectOnCancel = rejectOnCancel
+	}
+
+	cancel() {
+		this.canceled = true
+
+		if (_(this).rejectOnCancel) {
+			_(this).originalReject(new PromiseCancellation('canceled'))
+		}
+	}
+}
+
+class PromiseCancellation extends Error {}
 
 customAttributes.define('has', HasAttribute)
 
