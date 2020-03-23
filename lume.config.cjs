c677982e6b67799031e5838497586693ab24667a
Joe Pea <joe@trusktr.io>
2020-03-22 18:03:25 -0700

    remove unused lume config option

--- lume.config.cjs
+++ lume.config.cjs
@@ -1,9 +1,3 @@
 module.exports = {
 	globalName: false,
-	nodeModulesToCompile: [
-		// The build system ignores compiling anything in node_modules by default.
-		// List modules here (by name) that you want the build system to compile.
-
-		'custom-attributes',
-	],
 }
