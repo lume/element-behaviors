a371580a371f9d22fe078cc8225f6891c784677a
Joe Pea <joe@trusktr.io>
2021-02-17 08:41:19 -0800

    v2.2.7

--- src/index.ts
+++ src/index.ts
@@ -385,4 +385,4 @@ if (Element.prototype.attachShadow) {
 
 // Leave this last line alone, it gets automatically updated when publishing a
 // new version of this package.
-export const version = '2.2.6'
+export const version = '2.2.7'
