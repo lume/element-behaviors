fce189dabbb1767fcbca77a7dfd13f463c8c95c1
Joe Pea <joe@trusktr.io>
2022-02-20 20:48:57 -0800

    fix: make the has attribute type definition appear as an importable module or else consumers can't import the type, and it will instead be ambient/global which we don't want

--- src/attribute-types.ts
+++ src/attribute-types.ts
@@ -1,6 +1,10 @@
 // Import this file with `import type` if using `@lume/element` to register the
 // `has=""` attribute type for JSX templates.
 
+// @ts-ignore unused imported JSX type to make this file appear as an importable
+// module, rather than it being ambient/global.
+import type {JSX} from '@lume/element'
+
 declare module '@lume/element' {
 	namespace JSX {
 		interface CustomAttributes<T> {
