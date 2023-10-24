72a086df352852a71d5a6cbea1c674d2928fe247
Joe Pea <joe@trusktr.io>
2023-10-24 01:16:29 -0700

    v5.0.1

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 export * from './BehaviorMap.js';
 export * from './BehaviorRegistry.js';
 export * from './HasAttribute.js';
-export const version = '5.0.0';
+export const version = '5.0.1';
 //# sourceMappingURL=index.js.map
