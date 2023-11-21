fa295f6f995eb99de08d291780c1de567bba8ef9
Joe Pea <joe@trusktr.io>
2023-11-21 00:47:40 -0800

    v5.0.3

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 export * from './BehaviorMap.js';
 export * from './BehaviorRegistry.js';
 export * from './HasAttribute.js';
-export declare const version = "5.0.2";
+export declare const version = "5.0.3";
 //# sourceMappingURL=index.d.ts.map
