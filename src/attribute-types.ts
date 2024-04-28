2e3578d373f44fd98a0ed3684e1db568619bd6d2
Joe Pea <joe@trusktr.io>
2024-04-28 00:55:31 -0700

    add node about importing solid-js types or JSX types don't work

--- src/attribute-types.ts
+++ src/attribute-types.ts
@@ -1,3 +1,6 @@
+// I'm not sure why, but commenting this line out breaks all Solid JSX type
+// checking because all JSX types disappear except for the one from this file
+// (at time of writing).
 import type {} from 'solid-js'
 
 declare module 'solid-js' {
